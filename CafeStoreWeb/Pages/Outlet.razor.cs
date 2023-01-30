6c5140bb27407e4fa640e83d8fb80db5559de5e7
Pun-Mengly <punmengly168@gmail.com>
2023-01-30 13:58:32 +0700

    View and Post Outlet

--- CafeStoreWeb/Pages/Outlet.razor.cs
+++ CafeStoreWeb/Pages/Outlet.razor.cs
@@ -1,18 +1,31 @@
-﻿using Microsoft.AspNetCore.Components;
-using Microsoft.AspNetCore.Components.Rendering;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using Blazored.LocalStorage;
+using CafeStoreManagement.Models;
+using Radzen;
+using System.Net.Http.Headers;
+using System.Text.Json;
 
+namespace CafeStoreWeb.Pages;
 
-namespace CafeStoreWeb.Pages
+public partial class Outlet
 {
-    public partial class Outlet
+    IEnumerable<OutletModel> outlets;  
+    public  Outlet()
     {
-        public Outlet()
-        {
-
-        }
+    
+    }
+    public async Task GetOutletsAsync(HttpClient http, ILocalStorageService sessionStorage) {
+        string token = await sessionStorage.GetItemAsync<string>("Token");
+        var request = new HttpRequestMessage(HttpMethod.Get, "/api/Outlets");
+        request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", token);
+        var respone = await http.SendAsync(request);
+        var body = respone.Content.ReadAsStringAsync();
+        string result = body.Result;
+        outlets = JsonSerializer.Deserialize<List<OutletModel>>(result, new JsonSerializerOptions { PropertyNameCaseInsensitive = true })!;
+    }
+    public async Task OnSave(OutletModel model)
+    {
+        string token = await sessionStorage.GetItemAsync<string>("Token");
+        Http.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+        var response = await Http.PostAsJsonAsync("api/Outlets", model);
     }
 }
